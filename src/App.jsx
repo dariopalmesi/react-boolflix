2ccad0c3338ca7f4b63c7bcf3978f13d7f4f8c54
dariopalmesi <dariopalmesi88@gmail.com>
2024-11-28 15:59:37 +0100

    upgrade commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,54 @@ import '../node_modules/bootstrap-icons/font/bootstrap-icons.min.css'
 
 function App() {
   const [movies, setMovies] = useState({})
+  const [searchMovie, setSearchMovie] = useState('')
+  // const [movieFilter, setMovieFilter] = useState({})
+
+
+
   function fetchResult(url = 'https://api.themoviedb.org/3/search/movie?api_key=2b129c72fa5c2722b105bdcef3d40597&query=matrix') {
     fetch(url)
       .then(resp => resp.json())
-      .then(data => {
-        console.log(data);
-        setMovies(data)
+      .then(results => {
+        console.log(results);
+        setMovies(results)
 
       })
   }
-  function handleClick(e) {
+  function handleSearchClick() {
+    const movieFilter = movies.filter((movie) => movie.includes(searchMovie))
+    console.log(movieFilter);
+    setSearchMovie(movieFilter)
+
     fetchResult()
   }
-  // useEffect(fetchResult, [])
+  useEffect(fetchResult, [])
   return (
     <>
-      <button type='button' onClick={handleClick}>Fetch Characters</button>
+      <div className="container">
+        <div className="mb-3">
+          <label htmlFor="" className="form-label">Name</label>
+          <input
+            type="search"
+            className="form-control"
+            name="searchText"
+            id="searchText"
+            aria-describedby="searchHelper"
+            placeholder="Search..."
+          />
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={handleSearchClick}
+          >
+            Submit
+          </button>
+
+        </div>
+      </div>
+
+
+
 
       <section className='characters'>
         <div className="container">
@@ -30,10 +62,14 @@ function App() {
                 movies.results.map(movie => (
                   <div className="col" key={movie.id}>
                     <div className="card">
-                      <p>{movie.title}</p>
-                      <p>{movie.original_title}</p>
-                      <p>{movie.original_language}</p>
-                      <p>{movie.vote_average}</p>
+
+                      <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+                      <ul>
+                        <li><h2>{movie.title}</h2></li>
+                        <li>{movie.original_title}</li>
+                        <li>{movie.original_language}</li>
+                        <li>{movie.vote_average}</li>
+                      </ul>
                     </div>
                   </div>
                 )) :
