a62be49ec53a8063d1e52a4fe7c33a135799ee2e
dariopalmesi <dariopalmesi88@gmail.com>
2024-11-28 17:30:59 +0100

    upgrade componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,19 @@
 import { useEffect, useState } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.min.css';
+import { GlobalProvider } from './contexts/GlobalContext';
+import MoviesList from './components/moviesList';
 
 function App() {
-  const [movies, setMovies] = useState({});
-  const [searchMovie, setSearchMovie] = useState('');
 
-
-  function fetchResult(query) {
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=2b129c72fa5c2722b105bdcef3d40597&query=${query}`;
-    fetch(url)
-      .then((resp) => resp.json())
-      .then((results) => {
-        console.log(results);
-        setMovies(results);
-      })
-      .catch((error) => console.error('Errore nella chiamata API:', error));
-  }
-
-
-  function handleSearchClick() {
-
-    fetchResult(searchMovie);
-  }
 
   return (
     <>
-      <div className="container">
-        <div className="mb-3">
-          <label htmlFor="searchText" className="form-label">
-            Name
-          </label>
-          <input
-            type="search"
-            className="form-control"
-            name="searchText"
-            id="searchText"
-            aria-describedby="searchHelper"
-            placeholder="Search..."
-            value={searchMovie}
-            onChange={(e) => setSearchMovie(e.target.value)}
-          />
-          <button
-            type="submit"
-            className="btn btn-primary mt-2"
-            onClick={handleSearchClick}
-          >
-            Submit
-          </button>
-        </div>
-      </div>
+      <GlobalProvider>
+        <MoviesList />
+      </GlobalProvider>
 
-      <section className='characters'>
-        <div className="container">
-          <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3'>
-            {
-              movies.results ?
-                movies.results.map(movie => (
-                  <div className="col" key={movie.id}>
-                    <div className="card">
 
-                      <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-                      <ul>
-                        <li><h2>{movie.title}</h2></li>
-                        <li>{movie.original_title}</li>
-                        <li>{movie.original_language}</li>
-                        <li>{movie.vote_average}</li>
-                      </ul>
-                    </div>
-                  </div>
-                )) :
-                <p>No result yet</p>
-            }
-          </div>
-        </div>
-      </section>
     </>
   );
 }
--- src/contexts/GlobalContext.jsx
+++ src/contexts/GlobalContext.jsx
@@ -1,5 +1,56 @@
-import { createContext } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 
 const GlobalContext = createContext();
 
-export default GlobalContext;
+export const GlobalProvider = ({ Children }) => {
+    const [movies, setMovies] = useState({});
+    const [searchMovie, setSearchMovie] = useState('');
+
+
+    function fetchResult(query) {
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=2b129c72fa5c2722b105bdcef3d40597&query=${query}`;
+        fetch(url)
+            .then((resp) => resp.json())
+            .then((results) => {
+                console.log(results);
+                setMovies(results);
+            })
+            .catch((error) => console.error('Errore nella chiamata API:', error));
+    }
+
+
+    function handleSearchClick() {
+
+        fetchResult(searchMovie);
+    }
+    return (
+        <GlobalContext.Provider value={{ movies, setMovies, searchMovie, setSearchMovie }}>
+            <div className="container">
+                <div className="mb-3">
+                    <label htmlFor="searchText" className="form-label">
+                        Name
+                    </label>
+                    <input
+                        type="search"
+                        className="form-control"
+                        name="searchText"
+                        id="searchText"
+                        aria-describedby="searchHelper"
+                        placeholder="Search..."
+                        value={searchMovie}
+                        onChange={(e) => setSearchMovie(e.target.value)}
+                    />
+                    <button
+                        type="submit"
+                        className="btn btn-primary mt-2"
+                        onClick={handleSearchClick}
+                    >
+                        Submit
+                    </button>
+                </div>
+            </div>
+            {Children}
+        </GlobalContext.Provider>
+    )
+}
+export const movieGlobalContext = () => useContext(GlobalContext)
