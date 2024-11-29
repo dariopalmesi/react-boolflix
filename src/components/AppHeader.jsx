48f873d9d848ad4344eed1eeba98227ead768bf9
dariopalmesi <dariopalmesi88@gmail.com>
2024-11-29 17:20:57 +0100

    upgrade searchbar components commit

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,34 +1,9 @@
-import { useGlobalContext } from "../contexts/GlobalContext"
+import SearchBar from "./SearchBar"
 
 export default function AppHeader() {
-    const { searchMovie, setSearchMovie, handleSearchClick } = useGlobalContext()
+
 
     return (
-        <header className='bg-black'>
-            <div className="container d-flex justify-content-end align-items-center gap-3 p-3">
-                <div className="mb-3 d-flex align-items-center gap-3 ">
-                    <label htmlFor="searchText" className="form-label text-white">
-                        Scrivi la tua ricerca
-                    </label>
-                    <input
-                        type="search"
-                        className=""
-                        name="searchText"
-                        id="searchText"
-                        aria-describedby="searchHelper"
-                        placeholder="Search..."
-                        value={searchMovie}
-                        onChange={(e) => setSearchMovie(e.target.value)}
-                    />
-                    <button
-                        type="submit"
-                        className="btn btn-dark mt-2"
-                        onClick={handleSearchClick}
-                    >
-                        Cerca
-                    </button>
-                </div>
-            </div>
-        </header>
+        <SearchBar />
     )
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,33 @@
+import { useGlobalContext } from "../contexts/GlobalContext";
+
+export default function SearchBar() {
+    const { searchMovie, setSearchMovie, handleSearchClick } = useGlobalContext()
+    return (
+        <header className='bg-black'>
+            <div className="container d-flex justify-content-end align-items-center gap-3 p-3">
+                <div className="mb-3 d-flex align-items-center gap-3 ">
+                    <label htmlFor="searchText" className="form-label text-white">
+                        Scrivi la tua ricerca
+                    </label>
+                    <input
+                        type="search"
+                        className=""
+                        name="searchText"
+                        id="searchText"
+                        aria-describedby="searchHelper"
+                        placeholder="Search..."
+                        value={searchMovie}
+                        onChange={(e) => setSearchMovie(e.target.value)}
+                    />
+                    <button
+                        type="submit"
+                        className="btn btn-dark mt-2"
+                        onClick={handleSearchClick}
+                    >
+                        Cerca
+                    </button>
+                </div>
+            </div>
+        </header>
+    )
+}
